8d92162f4aedba1f1b7d1d981a5adffbe06afc96
Matt Bruzek <matthew.bruzek@canonical.com>
2016-12-01 20:02:41 -0600

    Changing the docstring.

--- FLOW018/python/small_factorial.py
+++ FLOW018/python/small_factorial.py
@@ -4,7 +4,7 @@ import sys
 
 
 def factorial(n):
-    '''Return the factorial value of the parameter n. '''
+    """Return the factorial value of the parameter n."""
     fact = 1
     # range(4, 1, -1) = [4, 3, 2]
     for x in range(n, 1, -1):
